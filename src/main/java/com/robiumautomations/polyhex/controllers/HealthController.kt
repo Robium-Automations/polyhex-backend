338b3066603b111efcbf418959661e0ec6f6fd0c
Taras Lavreniuk <taras@staffbase.com>
2020-03-07 13:45:23 +0100

    :hammer: Add /health endpoint

--- src/main/java/com/robiumautomations/polyhex/controllers/HealthController.kt
+++ src/main/java/com/robiumautomations/polyhex/controllers/HealthController.kt
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloWorldController {
+class HealthController {
 
   @GetMapping("/helloworld", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun helloWorld() = ResponseEntity(mapOf("Hello" to "world"), HttpStatus.OK)
 
   @GetMapping("/helloworld_protected", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun helloWorldProtected() = ResponseEntity(mapOf("Hello" to "world"), HttpStatus.OK)
+
+  @GetMapping("/health", produces = [MediaType.APPLICATION_JSON_VALUE])
+  fun health() = ResponseEntity("This shit does work!!!", HttpStatus.OK)
 }

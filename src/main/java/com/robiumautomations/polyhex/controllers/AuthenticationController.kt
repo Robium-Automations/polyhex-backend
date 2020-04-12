946b82a900b3759db335b1a2b7e34785087cac95
Taras Lavreniuk <taras@staffbase.com>
2020-04-12 21:32:58 +0200

    :zap: Remove token from response body in /signin

--- src/main/java/com/robiumautomations/polyhex/controllers/AuthenticationController.kt
+++ src/main/java/com/robiumautomations/polyhex/controllers/AuthenticationController.kt
@@ -1,17 +1,15 @@
 package com.robiumautomations.polyhex.controllers
 
-import com.robiumautomations.polyhex.dtos.users.SignInedUser
 import com.robiumautomations.polyhex.models.UserCredentials
 import com.robiumautomations.polyhex.security.CustomAuthentication
 import com.robiumautomations.polyhex.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.security.core.AuthenticationException
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AuthenticationController {
@@ -31,10 +29,9 @@ class AuthenticationController {
       return ResponseEntity("Specify password.", HttpStatus.BAD_REQUEST)
     }
     return try {
-      val token = customAuthentication.attemptAuthentication(user.username, user.password)
       return ResponseEntity.ok()
-          .header("Authorization", token)
-          .body(userService.getUserInfoByUsername(user.username)?.let { SignInedUser(token, it) })
+          .header("Authorization", customAuthentication.attemptAuthentication(user.username, user.password))
+          .body(userService.getUserInfoByUsername(user.username))
     } catch (e: AuthenticationException) {
       e.printStackTrace()
       ResponseEntity(mapOf("Message" to "Invalid credentials."), HttpStatus.BAD_REQUEST)

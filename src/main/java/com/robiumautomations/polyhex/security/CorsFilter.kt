ce5ac772cdf777b4030d1fc65646644cd0070c53
Taras Lavreniuk <taras@staffbase.com>
2020-04-13 13:01:39 +0200

    :zap: Adjust CORS stuff

--- src/main/java/com/robiumautomations/polyhex/security/CorsFilter.kt
+++ src/main/java/com/robiumautomations/polyhex/security/CorsFilter.kt
@@ -12,7 +12,7 @@ class CorsFilter : Filter {
 
   override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
     (response as HttpServletResponse).setHeader("Access-Control-Allow-Origin", "*")
-//    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
+    response.setHeader("Access-Control-Allow-Methods", "POST,PUT,GET,OPTIONS,DELETE")
     response.setHeader("Access-Control-Max-Age", "3600")
     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
     response.setHeader("Access-Control-Expose-Headers", "Content-Type, Authorization, Message")

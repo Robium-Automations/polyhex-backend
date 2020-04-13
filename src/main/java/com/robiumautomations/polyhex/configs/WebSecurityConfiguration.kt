4bd08844b3e709e6ae658036b6115797f1bbb241
Taras Lavreniuk <taras@staffbase.com>
2020-04-13 11:54:10 +0200

    :zap: Change CORS stuff

--- src/main/java/com/robiumautomations/polyhex/configs/WebSecurityConfiguration.kt
+++ src/main/java/com/robiumautomations/polyhex/configs/WebSecurityConfiguration.kt
@@ -4,23 +4,25 @@ import com.robiumautomations.polyhex.security.JwtAuthenticationFilter
 import com.robiumautomations.polyhex.security.JwtAuthorizationFilter
 import com.robiumautomations.polyhex.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.web.cors.CorsConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.firewall.HttpFirewall
+import org.springframework.security.web.firewall.StrictHttpFirewall
+import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.web.firewall.StrictHttpFirewall
-import org.springframework.security.web.firewall.HttpFirewall
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -67,6 +69,20 @@ open class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
       it.setAllowUrlEncodedSlash(true)
       it.setAllowSemicolon(true)
     }
+  }
+
+  @Bean
+  open fun corsFilter(): FilterRegistrationBean<*> {
+    val source = UrlBasedCorsConfigurationSource()
+    val config = CorsConfiguration()
+    config.allowCredentials = true
+    config.addAllowedOrigin("*")
+    config.addAllowedHeader("*")
+    config.addAllowedMethod("*")
+    source.registerCorsConfiguration("/**", config)
+    val bean: FilterRegistrationBean<*> = FilterRegistrationBean(CorsFilter(source))
+    bean.order = 0
+    return bean
   }
 
   override fun configure(web: WebSecurity) {
--- src/main/java/com/robiumautomations/polyhex/security/CorsFilter.kt
+++ src/main/java/com/robiumautomations/polyhex/security/CorsFilter.kt
@@ -1,21 +1,21 @@
-package com.robiumautomations.polyhex.security
-
-import org.springframework.stereotype.Component
-import javax.servlet.Filter
-import javax.servlet.FilterChain
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
-import javax.servlet.http.HttpServletResponse
-
-@Component
-class CorsFilter : Filter {
-
-  override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-    (response as HttpServletResponse).setHeader("Access-Control-Allow-Origin", "*")
-    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
-    response.setHeader("Access-Control-Max-Age", "3600")
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
-    response.setHeader("Access-Control-Expose-Headers", "Content-Type, Authorization, Message")
-    chain!!.doFilter(request, response)
-  }
-}
+//package com.robiumautomations.polyhex.security
+//
+//import org.springframework.stereotype.Component
+//import javax.servlet.Filter
+//import javax.servlet.FilterChain
+//import javax.servlet.ServletRequest
+//import javax.servlet.ServletResponse
+//import javax.servlet.http.HttpServletResponse
+//
+//@Component
+//class CorsFilter : Filter {
+//
+//  override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+//    (response as HttpServletResponse).setHeader("Access-Control-Allow-Origin", "*")
+//    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
+//    response.setHeader("Access-Control-Max-Age", "3600")
+//    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
+//    response.setHeader("Access-Control-Expose-Headers", "Content-Type, Authorization, Message")
+//    chain!!.doFilter(request, response)
+//  }
+//}
